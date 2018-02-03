875acfde8f40b53881d8d72e743d971b150cceda
JiaqiHe <hjq53589793@outlook.com>
2018-02-02 20:59:38 -0800

    optional

--- flix/NowPlayingViewController.swift
+++ flix/NowPlayingViewController.swift
@@ -8,23 +8,31 @@
 
 import UIKit
 import AlamofireImage
+import PKHUD
 
-class NowPlayingViewController: UIViewController, UITableViewDataSource {
+class NowPlayingViewController: UIViewController, UITableViewDataSource, UISearchBarDelegate {
 
-    @IBOutlet weak var tableView:
-        UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    let alertController = UIAlertController(title: "Cannot Get Movies", message: "The Internet connection seems to be offline.", preferredStyle: .alert)
+    
+    
     
     var movies: [[String: Any]] = []
     var refreshControl: UIRefreshControl!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        self.tableView.rowHeight = 200
         refreshControl = UIRefreshControl()
         refreshControl.addTarget(self, action: #selector(NowPlayingViewController.didPullToRefresh(_:)), for: .valueChanged)
         tableView.insertSubview(refreshControl, at: 0)
         
         tableView.dataSource = self
+        self.searchBar.delegate = self
         fetchMovies()
         
         
@@ -35,6 +43,7 @@ class NowPlayingViewController: UIViewController, UITableViewDataSource {
     }
     
     func fetchMovies() {
+        activityIndicator.startAnimating()
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
@@ -42,21 +51,29 @@ class NowPlayingViewController: UIViewController, UITableViewDataSource {
             // This will run when the network request returns
             if let error = error {
                 print(error.localizedDescription)
+                let action = UIAlertAction(title: "OK", style: .default) { (action:UIAlertAction) in
+                    print("You've pressed OK");
+                }
+                self.alertController.addAction(action)
+                self.present(self.alertController, animated: true, completion: nil)
             } else if let data = data {
                 let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
                 self.movies = dataDictionary["results"] as! [[String: Any]]
                 self.tableView.reloadData()
                 self.refreshControl.endRefreshing()
+                self.activityIndicator.stopAnimating()
             }
         }
         task.resume()
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return movies.count
+        if(isSearch) {return searchResult.count}
+        else {return movies.count}
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as! MovieCell
+        if (isSearch == false) {
         let movie = movies[indexPath.row]
         let title = movie["title"] as! String
         let overview = movie["overview"] as! String
@@ -68,6 +85,20 @@ class NowPlayingViewController: UIViewController, UITableViewDataSource {
         cell.overviewLabel.text = overview
         cell.posterImageView.af_setImage(withURL: posterURL)
         return cell
+        }
+        else {
+            let movie = searchResult[indexPath.row]
+            let title = movie["title"] as! String
+            let overview = movie["overview"] as! String
+            let posterpass = movie["poster_path"] as! String
+            
+            let baseURL = "https://image.tmdb.org/t/p/w500"
+            let posterURL = URL(string: baseURL + posterpass)!
+            cell.titleLabel.text = title
+            cell.overviewLabel.text = overview
+            cell.posterImageView.af_setImage(withURL: posterURL)
+            return cell
+        }
     }
     
     override func didReceiveMemoryWarning() {
@@ -75,7 +106,67 @@ class NowPlayingViewController: UIViewController, UITableViewDataSource {
         // Dispose of any resources that can be recreated.
     }
     
-
+    
+    var isSearch : Bool = false
+    var searchResult : [[String: Any]] = []
+    //MARK: UISearchbar delegate
+    func searchBarTextDidBeginEditing(searchBar: UISearchBar) {
+        isSearch = true;
+        self.tableView.reloadData()
+    }
+    
+    func searchBarTextDidEndEditing(searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+        isSearch = false;
+        searchResult = []
+        self.tableView.reloadData()
+    }
+    
+    func searchBarCancelButtonClicked(searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+        isSearch = false;
+        searchResult = []
+        self.tableView.reloadData()
+    }
+    
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+        isSearch = false;
+        searchResult = []
+        self.tableView.reloadData()
+    }
+    
+    func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        if searchText.characters.count == 0 {
+            isSearch = false;
+            self.tableView.reloadData()
+        } else {
+            searchResult = []
+            for movie in movies {
+                let tmp = movie["title"] as! String
+                if tmp.lowercased().range(of:searchText) != nil {
+                    searchResult.append(movie)
+                }
+            }
+        }
+        if(searchResult.count == 0){
+            isSearch = false;
+        } else {
+            isSearch = true;
+        }
+        self.tableView.reloadData()
+    }
+    
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        self.searchBar.showsCancelButton = true
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.showsCancelButton = false
+        searchBar.text = ""
+        searchBar.resignFirstResponder()
+    }
     /*
     // MARK: - Navigation
 

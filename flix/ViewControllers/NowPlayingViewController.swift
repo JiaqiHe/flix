173062b529a6fd4ba9c0ef59fa77cdd9672b4b9c
JiaqiHe <hjq53589793@outlook.com>
2018-02-06 00:11:26 -0800

    attempt

--- flix/ViewControllers/NowPlayingViewController.swift
+++ flix/ViewControllers/NowPlayingViewController.swift
@@ -10,10 +10,12 @@ import UIKit
 import AlamofireImage
 import PKHUD
 
-class NowPlayingViewController: UIViewController, UITableViewDataSource {
+class NowPlayingViewController: UIViewController, UITableViewDataSource, UISearchBarDelegate {
 
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var searchBlock: UISearchBar!
+    
     
     var movies: [[String: Any]] = []
     var refreshControl: UIRefreshControl!
@@ -22,22 +24,67 @@ class NowPlayingViewController: UIViewController, UITableViewDataSource {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        // search bar
+        searchBlock.delegate = self
+        
+        // HUD
         HUD.dimsBackground = false
         HUD.allowsInteraction = false
         
+        // table cell set
         self.tableView.rowHeight = 160
+        
+        // refresh control
         refreshControl = UIRefreshControl()
         refreshControl.addTarget(self, action: #selector(NowPlayingViewController.didPullToRefresh(_:)), for: .valueChanged)
         tableView.insertSubview(refreshControl, at: 0)
         
         tableView.dataSource = self
         fetchMovies()
-        
-        
     }
 
     @objc func didPullToRefresh(_ refreshControl: UIRefreshControl) {
         fetchMovies()
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if (searchText.isEmpty) {
+            fetchMovies()
+        }
+        else {
+            let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
+            let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+            let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+            let task = session.dataTask(with: request) { (data, response, error) in
+                // This will run when the network request returns
+                if let error = error {
+                    print(error.localizedDescription)
+                    // network error alert
+                    let alertController = UIAlertController(title: "Cannot Get Movies", message: "The Internet connection appears to be offline.", preferredStyle: .alert)
+                    let OKAction = UIAlertAction(title: "OK", style: .default) { (action) in
+                        // handle response here.
+                        
+                    }
+                    // add the OK action to the alert controller
+                    alertController.addAction(OKAction)
+                    self.present(alertController, animated: true) {
+                        // optional code for what happens after the alert controller has finished presenting
+                    }
+                } else if let data = data {
+                    let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                    let allMovies = dataDictionary["results"] as! [[String: Any]]
+                    //==================search==================
+                    for movie in allMovies {
+                        let title = movie["title"] as! String
+                        if (title.range(of: searchText, options: .caseInsensitive, range: nil, locale: nil) != nil) {
+                            self.movies.append(movie)
+                        }
+                    }
+                    self.tableView.reloadData()
+                }
+            }
+            task.resume()
+        }
     }
     
     func fetchMovies() {

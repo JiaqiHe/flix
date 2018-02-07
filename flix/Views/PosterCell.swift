af8b6c9358c8b471f302c6fc9556fe3f07d2b11c
JiaqiHe <hjq53589793@outlook.com>
2018-02-06 19:34:18 -0800

    add superhero page

---
+++ flix/Views/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  flix
+//
+//  Created by Jiaqi He on 2/6/18.
+//  Copyright © 2018 Jiaqi He. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+}
